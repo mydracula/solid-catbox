12fa9387ca06628a53dde2fd8aff462371ac35d6
2458578637 <2458578637@qq.com>
2023-08-31 10:59:40 +0800

    ok

--- src/routes/api/upload.jsx
+++ src/routes/api/upload.jsx
@@ -1,5 +1,33 @@
 import { APIEvent, json } from "solid-start/api";
 
+
+
+export const config = {
+  runtime: "edge",
+  regions: [
+    "hkg1",
+    "sin1",
+    "kix1",
+    "icn1",
+    "bom1",
+    "hnd1",
+    "arn1",
+    "bru1",
+    "cdg1",
+    "cle1",
+    "cpt1a",
+    "dub1",
+    "fra1",
+    "gru1",
+    "iad1",
+    "lhr1",
+    "pdx1",
+    "sfo1",
+    "syd1"
+  ]
+}
+
+
 export async function POST(context) {
   const body = await context.request.formData()
   const data = await (await fetch(`https://tuphp.hicks.workers.dev/`,
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,16 @@
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
+import vercel from 'solid-start-vercel'
+
+
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-    hmr: true,
-    proxy: {
-      '/dev': {
-        target: 'https://tuphp.hicks.workers.dev/',
-        https: false,
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/dev/, ''),
-        bypass (req, res, opt) {
-          const proxyUrl =
-            new URL(opt.rewrite(req.url) || '', opt.target)?.href || ''
-          res.setHeader('x-res-proxyUrl', proxyUrl)
-        }
-      }
-    }
-  },
-  plugins: [solid()]
+  plugins: [
+    solid({
+      inspect: false,
+      ssr: true
+      adapter: vercel({
+        edge: true
+      })
+    })
+  ]
 })
