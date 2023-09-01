e385bbceba79939c32bba3ec0de22cc850c9441a
2458578637 <2458578637@qq.com>
2023-09-01 17:04:00 +0800

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 import vercel from 'solid-start-vercel'
+import netlify from 'solid-start-netlify'
 
+const adapter = () => {
+  if (process.env.VERCEL) {
+    return vercel({ edge: true })
+  } else if (process.env.NETLIFY) {
+    return netlify({ edge: true })
+  } else {
+    return 'node'
+  }
+}
 
 export default defineConfig({
   plugins: [
     solid({
       inspect: false,
       ssr: true,
-      adapter: vercel({
-        edge: true
-      })
+      adapter: adapter()
     })
   ]
 })
